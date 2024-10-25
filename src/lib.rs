bc233c6aab8a5b6ac7cdb64362a6eec3486506ca
Dima Tisnek <dimaqq@gmail.com>
2024-10-25 15:41:45 +0700

    feat: errors!

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,52 @@
 extern crate pyo3 as pyo3_crate;
 use pyo3_crate::prelude::*;
+use std::fs::File;
+use std::io::Read;
+use pyo3_crate::exceptions::PyFileNotFoundError;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b + 100).to_string())
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn say_hello(name: String) -> PyResult<String> {
+    Ok(format!("Hello {}", name))
+}
+
+#[pyfunction]
+#[pyo3(signature = (name, conf="Pycon APAC".to_string()))]
+fn say_more_hello(name: String, conf: String) -> PyResult<String> {
+    Ok(format!("Hello {}, welcome to {}", name, conf))
+}
+
+#[pyfunction]
+fn check_reg(filename: String, name: String) -> PyResult<String> {
+    let afile = File::open(filename);
+    match afile {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)?;
+            if contents.contains(&name) {
+                Ok("You're OK".to_string())
+            } else {
+                Ok("Sorry, nope".to_string())
+            }
+        },
+        Err(_) => {
+            // TODO: handle different kinds of errors here
+            Err(PyFileNotFoundError::new_err("No such file or directory"))
+        },
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyo3(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(say_hello, m)?)?;
+    m.add_function(wrap_pyfunction!(say_more_hello, m)?)?;
+    m.add_function(wrap_pyfunction!(check_reg, m)?)?;
     Ok(())
 }
--- test/test_lib.py
+++ test/test_lib.py
@@ -1,3 +1,7 @@
+from typing import reveal_type
+
+import pytest
+
 import pyo3
 
 
@@ -5,5 +9,20 @@ def test_add_function():
     assert pyo3.sum_as_string(1, 2) == "3"
 
 
+@pytest.mark.skip(reason="xxx")
 def test_add_huge_ints():
     assert pyo3.sum_as_string(0x23456789123456789123456789, 1) == "..."
+
+
+reveal_type(pyo3.say_hello)
+
+
+def test_check_bad_file():
+    with pytest.raises(FileNotFoundError):
+        assert pyo3.check_reg("/etc/FOOBAR", "FOOBAR") == "xx"
+
+def test_check_user():
+    assert pyo3.check_reg("/etc/passwd", "nobody") == "You're OK"
+
+def test_check_no():
+    assert pyo3.check_reg("/etc/passwd", "FOOBAR") == "Sorry, nope"
