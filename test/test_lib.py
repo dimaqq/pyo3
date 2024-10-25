c74fde95f0b2b933fbe3e1b48244aab10dd9acdb
Dima Tisnek <dimaqq@gmail.com>
2024-10-25 15:56:38 +0700

    feat: types!... manually

--- test/test_lib.py
+++ test/test_lib.py
@@ -1,4 +1,4 @@
-from typing import reveal_type
+from typing import reveal_type as reveal_type
 
 import pytest
 
@@ -9,12 +9,21 @@ def test_add_function():
     assert pyo3.sum_as_string(1, 2) == "3"
 
 
-@pytest.mark.skip(reason="xxx")
+def test_add_function_kw1():
+    assert pyo3.sum_as_string(a=1, b=2) == "3"
+
+
+@pytest.mark.xfail(reason="wrong arg name")
+def test_add_function_kw2():
+    assert pyo3.sum_as_string(x=1, y=2) == "3"  # type: ignore
+
+
+@pytest.mark.skip(reason="value is too large, anyway")
 def test_add_huge_ints():
     assert pyo3.sum_as_string(0x23456789123456789123456789, 1) == "..."
 
 
-reveal_type(pyo3.say_hello)
+# reveal_type(pyo3.say_hello)
 
 
 def test_check_bad_file():
